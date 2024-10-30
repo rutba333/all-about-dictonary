be40b9e2abd635c247c2ea00fad6a2628b33b470
rutba333 <sa2497589@gmail.com>
2024-10-30 21:16:58 +0600

    activity-3

---
+++ activity-3.py
@@ -0,0 +1,12 @@
+country_code={'India':'0091',
+                 'Australia':'0025',
+                 'Nepal':'00977'}
+
+#search dictonary for country code in india
+
+print("country code for india-")
+print(country_code.get('Imdia','Not found'))
+
+#search dictonary for country code in japan
+print("country code for japan-")
+print(country_code.get('japan','not found'))
